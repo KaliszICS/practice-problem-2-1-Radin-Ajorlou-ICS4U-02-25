eb0ec4d1160a64f6a6398eb1a8ec5077ef42a6fd
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-03-25 12:28:31 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,49 @@ public class PracticeProblem {
 
 	}
 
+
+
+
+
+	public static int find(int[] array, int number){
+		for (int i = 0 ; i < array.length; i++){
+			if (array[i] == number)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+
+
+	public static int findLast(String[] array, String string){
+		for (int i = (array.length - 1); i >= 0; i--){
+			if (array[i] == string){
+				return (i);
+			}
+		}
+		return -1;
+	}
+
+
+
+
+	public static int findSecond(char[] array, char character){
+		int counter = 0; 
+		int recentOccurence = -1;
+		for (int i = 0; i < array.length; i++){
+			if (array[i] == character){
+				recentOccurence = i;
+				counter++;
+			}
+			if (counter == 2){
+				break;
+			}
+		}
+		return recentOccurence;
+
+	}
+
 	
 
 }
